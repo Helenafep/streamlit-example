c905499d0bfa98414798db3b2ce200ef45ad7832
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 05:32:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ with open('./hackupc2023_restbai__dataset_sample.json') as f:
     data = json.load(f)
 
 df = pd.DataFrame(data).T
+st.image(df.iloc[0, 'images'])
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
