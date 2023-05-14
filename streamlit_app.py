7f63fea3f95cabfc2d5371f19ad48d04ee21f3d3
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:25:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,17 +29,3 @@ IMAGES = [
     "https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920",
     "https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920",
 ]
-
-
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
