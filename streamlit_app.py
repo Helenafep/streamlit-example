06757b7ca4da5de5ec9e636d8b5d275660136ed1
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:54:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,8 @@ import json
 
 # Read CSV file into a pandas DataFrame
 df = pd.read_csv("./my_data.csv")
-print(df)
-IMAGES = [
-    'https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg',
-    'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
-    'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
-]
-for image_file in IMAGES:
+imgs = df['images']
+for image_file in imgs:
     st.image(image_file, caption=image_file, use_column_width=True)
     
 
