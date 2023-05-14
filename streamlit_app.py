6e1419c676e94385554eb042badd1a9abb342c2a
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:56:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import json
 
 # Read CSV file into a pandas DataFrame
 df = pd.read_csv("./my_data.csv")
-imgs = df['images']
+imgs = df['images',1]
 for image_file in imgs:
     st.image(image_file, caption=image_file, use_column_width=True)
     
