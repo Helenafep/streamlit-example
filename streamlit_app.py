43c235f76b10feb7d1f46e678928deff8896f8a0
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:51:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import json
 
 
 # Read CSV file into a pandas DataFrame
-df = pd.read_csv("data.csv")
+df = pd.read_csv("./my_data.csv")
 
 IMAGES = [
     'https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg',
