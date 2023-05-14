68b25cf69457c6a84f7ec09829f65de7b5160716
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:48:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,8 @@ import json
 import pandas as pd
 
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+data = read.csv('./my_data.csv')
+df = pd.DataFrame(data).T
 IMAGES = [
     'https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg',
     'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
@@ -25,9 +17,4 @@ IMAGES = [
 for image_file in IMAGES:
     st.image(image_file, caption=image_file, use_column_width=True)
     
-df = pd.DataFrame(data).T
 
-
-
-
-show_photos(IMAGES)
