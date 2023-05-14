a0cdd1bd0b156e7fffc3949e646146c8b4a67286
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:35:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ df = pd.DataFrame(data).T
 st.image('https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg')
 st.image("https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920")
 IMAGES = [
-    'https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg',
+    'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
     'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
 ]
 
