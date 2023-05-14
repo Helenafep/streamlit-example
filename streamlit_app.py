088471ecff9e1d75962d324f25fb4fdb38d0cc80
Helenafep <113341925+Helenafep@users.noreply.github.com>
2023-05-14 06:44:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,41 +17,17 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
-with open('./hackupc2023_restbai__dataset_sample.json') as f:
-    data = json.load(f)
-
-df = pd.DataFrame(data).T
-st.image('https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg')
-st.image("https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920")
 IMAGES = [
     'https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg',
     'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
     'https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920',
 ]
-import streamlit as st
-from PIL import Image
-
-def show_photos(photos_list):
-    """
-    A function that takes a list of photos and displays them in a grid.
+for image_file in IMAGES:
+    st.image(image_file, caption=image_file, use_column_width=True)
     
-    Parameters:
-        photos_list (list): A list of photo file paths or URLs.
-    """
-    
-    # Check if photos_list is empty
-    if not photos_list:
-        st.warning("No photos to display.")
-        return
-    
-    # Loop through the list of photos and display each photo in a grid
-    for photo in photos_list:
-        try:
-            image = Image.open(photo)
-            st.image(image, caption=photo)
-        except Exception as e:
-            st.warning(f"Could not display {photo}. {str(e)}")
+df = pd.DataFrame(data).T
+st.image('https://restb-hackathon.s3.amazonaws.com/real_estate_dataset/images/303464__013.jpg')
+st.image("https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920")
 
 
 
